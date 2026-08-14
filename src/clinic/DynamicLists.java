a8f3630bcbe4894725bf772e45b89389072a45c4
Kevin <kevinmejias866@gmail.com>
2026-08-14 14:52:42 -0600

    Interfaz DynamicLists
    Se desarrolla una nueva interfaz en el paquete clinic

---
+++ src/clinic/DynamicLists.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package clinic;
+
+import java.util.Iterator;
+
+/**
+ *
+ * @author kevin
+ * @param <T>
+ * @param <K>
+ */
+public interface DynamicLists <T> {
+    public boolean add(T item);
+    public Iterator getAll();
+    public int size();
+    public boolean isEmpty();
+}
